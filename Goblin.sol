b1b8c1eef982e0ab0bc465cf484dfb73ee195a50
Trankuilized <mvinathan@gmail.com>
2023-04-24 03:51:04 +0000

    add pausable

--- Goblin.sol
+++ Goblin.sol
@@ -4,13 +4,19 @@ pragma solidity ^0.8.9;
 import "./@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "./@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import "./@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
-import "./@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "./@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "./@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
 import "./@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 import "./@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20FlashMintUpgradeable.sol";
 import "./@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract Goblin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, ERC20FlashMintUpgradeable {
+/// @custom:security-contact security@goblins.cash
+contract Goblin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, AccessControlUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, ERC20FlashMintUpgradeable {
+    bytes32 public constant SNAPSHOT_ROLE = keccak256("SNAPSHOT_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -20,30 +26,44 @@ contract Goblin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ER
         __ERC20_init("Goblin", "GOB");
         __ERC20Burnable_init();
         __ERC20Snapshot_init();
-        __Ownable_init();
+        __AccessControl_init();
+        __Pausable_init();
         __ERC20Permit_init("Goblin");
         __ERC20Votes_init();
         __ERC20FlashMint_init();
 
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(SNAPSHOT_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
         _mint(msg.sender, 35000 * 10 ** decimals());
+        _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function snapshot() public onlyOwner {
+    function snapshot() public onlyRole(SNAPSHOT_ROLE) {
         _snapshot();
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    // The following functions are overrides required by Solidity.
-
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
+        whenNotPaused
         override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
     {
         super._beforeTokenTransfer(from, to, amount);
     }
+
+    // The following functions are overrides required by Solidity.
 
     function _afterTokenTransfer(address from, address to, uint256 amount)
         internal
